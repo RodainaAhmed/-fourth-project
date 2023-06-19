010b8c027978b82e7494a000486a5498e1943cdc
RodainaAhmed <136331463+RodainaAhmed@users.noreply.github.com>
2023-06-20 00:10:15 +0300

    updated

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Heart)
-    basic.pause(50)
+    basic.pause(5000)
     basic.showLeds(`
         . . . . .
         . # . # .
