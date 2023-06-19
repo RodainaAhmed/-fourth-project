1501ad73557fbad71a0fd57daf601129b82e2949
RodainaAhmed <136331463+RodainaAhmed@users.noreply.github.com>
2023-06-19 23:57:10 +0300

    update

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(50)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(30)
